202a48b6c8167fb8f068bd3968b5ea4c8be3f0fc
ncalifano <niccolo.califano19@gmail.com>
2025-02-28 17:04:17 +0100

    fixed relative paths and improved formatting

--- scripts/shell/clean.sh
+++ scripts/shell/clean.sh
@@ -1,2 +1,4 @@
 SCRIPT_DIR="$(dirname $0)"
-rm "$SCRIPT_DIR/../results/fastqc/"*.{html,zip} "$SCRIPT_DIR/../results/minimap/"*.sam "$SCRIPT_DIR/../results/picard/"*.{bam,bai,txt}
+rm "$SCRIPT_DIR/../../results/fastqc/"*.{html,zip} \
+   "$SCRIPT_DIR/../../results/minimap/"*.sam \
+   "$SCRIPT_DIR/../../results/picard/"*.{bam,bai,txt}
