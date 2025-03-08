74bc02d903974d65b3f45d690e4ebbae2119da9c
ncalifano <niccolo.califano19@gmail.com>
2025-03-08 18:26:56 +0100

    fixed clean.sh to account for multiple multiqc_dataXXX folders

--- scripts/shell/clean.sh
+++ scripts/shell/clean.sh
@@ -2,6 +2,6 @@ SCRIPT_DIR="$(dirname $0)"
 rm -rf "$SCRIPT_DIR/../../results/fastqc/"*.{html,zip} \
    "$SCRIPT_DIR/../../results/minimap/"*.sam \
    "$SCRIPT_DIR/../../results/picard/"*.{bam,bai,txt} \
-   "$SCRIPT_DIR/../../results/multiqc/"{*.html,multiqc_data/} \
+   "$SCRIPT_DIR/../../results/multiqc/"{*.html,multiqc_data*/} \
    "$SCRIPT_DIR/../../raw_data/ref_data/"*.{dict,fa,fai} \
    "$SCRIPT_DIR/../../results/gatk/"*.vcf* \
