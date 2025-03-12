16054f052e96176c15cadc829a26a210e144deec
ncalifano <niccolo.califano19@gmail.com>
2025-03-12 17:16:54 +0100

    added removal of .bed files from raw_data/ref_data folder in clean.sh

--- scripts/shell/clean.sh
+++ scripts/shell/clean.sh
@@ -3,5 +3,5 @@ rm -rf "$SCRIPT_DIR/../../results/fastqc/"*.{html,zip} \
    "$SCRIPT_DIR/../../results/minimap/"*.sam \
    "$SCRIPT_DIR/../../results/picard/"*.{bam,bai,txt} \
    "$SCRIPT_DIR/../../results/multiqc/"{*.html,multiqc_data*/} \
-   "$SCRIPT_DIR/../../raw_data/ref_data/"*.{dict,fa,fai} \
+   "$SCRIPT_DIR/../../raw_data/ref_data/"*.{dict,fa,fai,bed} \
    "$SCRIPT_DIR/../../results/gatk/"*.vcf* \
